5f697fe2cf01f42c79f80c2924922006fd2775c2
saigetset <41384507+saigetset@users.noreply.github.com>
2023-05-24 01:16:10 -0500

    Update JDBCUtil.java

--- src/main/java/com/sai/util/JDBCUtil.java
+++ src/main/java/com/sai/util/JDBCUtil.java
@@ -6,6 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 import java.io.*;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class JDBCUtil {
 	private static Connection con=null;
 	static {
@@ -17,11 +20,16 @@ public class JDBCUtil {
 		}
 	}
 	public static Connection getJDBCConnection() throws SQLException, IOException {
-		FileInputStream fis = new FileInputStream("D:\\AdvJavaPrograms\\ServletApps\\JDBCCRUDAppUsingServlets\\src\\main\\java\\com\\sai\\resources\\application.properties");
-		Properties props = new Properties();
-		props.load(fis);
-		con = DriverManager.getConnection(props.getProperty("url"),props.getProperty("user"),props.getProperty("password"));
+		/*
+		 * FileInputStream fis = new FileInputStream(
+		 * "D:\\AdvJavaPrograms\\ServletApps\\JDBCCRUDAppUsingServlets\\src\\main\\java\\com\\sai\\resources\\application.properties"
+		 * ); Properties props = new Properties(); props.load(fis);
+		 */
+		HikariConfig config = new HikariConfig("D:\\AdvJavaPrograms\\ServletApps\\JDBCCRUDAppUsingServlets\\src\\main\\java\\com\\sai\\resources\\application.properties");
+		HikariDataSource dataSource = new HikariDataSource(config);
+		return dataSource.getConnection();
+		//con = DriverManager.getConnection(props.getProperty("url"),props.getProperty("user"),props.getProperty("password"));
 		//System.out.println("connected to the database");
-		return con;
+		//return con;
 	}
 }
